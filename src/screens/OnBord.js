53977f23e4578317fc33fb0ababa1ebfa02257a7
熊谷　佳紀 <yoshiki.kumagai@impact-taki.co.jp>
2023-02-16 15:06:26 +0900

    オンボーディング画面　完成

--- src/screens/OnBord.js
+++ src/screens/OnBord.js
@@ -5,90 +5,78 @@ import CommonStyles from "./CommonStyles.css";
 import Header from "../components/Header";
 // import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { Navigate } from "react-router-dom";
+import OnbordContent from "../components/OnbordContent";
+import OnbordNav from "../components/OnbordNav";
 
-const SignInPage = () => {
+import onbord3 from "../assets/images/onbord_3.svg";
+import onbord2 from "../assets/images/onbord_2.svg";
+import onbord1 from "../assets/images/onbord_1.svg";
+
+const OnBord = () => {
+  const contentArray = [onbord1, onbord2, onbord3];
+  const contentArrayLength = contentArray.length - 1;
+  const [contentCurrent, setContentCurrent] = useState(0);
+  const [onbordNavActive, setOnbordNavActive] = useState(0);
+  const onbordNavChange = (index) => {
+    setContentCurrent(index);
+    setOnbordNavActive(index);
+  };
+
   return (
-    <div className={CommonStyles.wrap}>
+    <div className={CommonStyles.wrap} style={styles.base}>
       <Header currentPage="オンボーディング" user="" />
       <div style={styles.wrap}>
-        <p>
-          <a href={"/memolist/"}>メモ一覧ページへ</a>
-        </p>
-        <p>
-          <a href={"/auth/"}>アカウント設定ページへ</a>
-        </p>
+        <OnbordContent
+          contentSrc={contentArray[contentCurrent]}
+          contentCurrent={contentCurrent}
+          contentArrayLength={contentArrayLength}
+        />
+        <ul style={styles.navList}>
+          {contentArray.map((item, index) => (
+            <OnbordNav
+              key={index}
+              style={styles.navItem}
+              navIndex={index}
+              onbordNavActive={onbordNavActive}
+              onClick={(index) => onbordNavChange(index)}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
-  // }
 };
 
-export default SignInPage;
+export default OnBord;
 
 const styles = {
   base: {
+    width: "100vw",
+    height: "100vh",
     background: "#fafafa",
     padding: "0px",
+    paddingTop: "11vw",
+    boxSizing: "border-box",
     margin: "0",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
   },
   wrap: {
-    padding: "0px",
-    margin: "38vw auto 0",
-    width: "79%",
+    padding: "10vw 8vw",
+    margin: "0",
+    width: "80%",
     borderRadius: "10px",
-  },
-  title: {
-    fontSize: "24px",
-    textAlign: "center",
-    width: "100%",
-    marginBottom: "40px",
-  },
-  labelText: {
-    color: "rgba(67,67,67,0.4)",
-    fontSize: "3vw",
-  },
-  inputText: {
-    width: "100vw",
-    margin: "0 calc(50% - 50vw)",
-    padding: "5vw 13% 4vw",
+    backgroundColor: "#fff",
     boxSizing: "border-box",
-    fontSize: "4.7vw",
-    marginTop: "1vw",
-    fontWeight: "bold",
   },
-  passwordBlock: {
-    marginTop: "13vw",
+  navList: {
+    width: "37%",
+    margin: "0 auto",
+    display: "flex",
+    justifyContent: "space-between",
   },
-  passwordAttentionText: {
-    margin: "0 calc(50% - 50vw)",
-    fontSize: "3vw",
-    marginTop: "1vw",
-    padding: "0 3vw",
-    textAlign: "right",
-  },
-  submitButton: {
-    width: "49vw",
-    margin: "11vw auto 0px",
-    display: "block",
-    padding: "3vw 0",
-    boxSizing: "border-box",
-    textAlign: "center",
-    fontSize: "5vw",
-    fontWeight: "bold",
-    border: "2px solid #5bcbcb",
-    color: "#5bcbcb",
-    borderRadius: "8px",
-    background: "#fff",
-  },
-  signUptext: {
-    textAlign: "center",
-    marginTop: "18vw",
-    fontWeight: "bold",
-  },
-  signUpLink: {
-    color: "#579DDD",
+  navItem: {
+    width: "25%",
   },
 };
